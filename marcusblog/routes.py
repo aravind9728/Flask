53ac6a15985ab856b0f51b30754bfd19b730be59
aravind9728 <aravind.sekar9797@gmail.com>
2020-08-17 02:09:48 +0530

    'bgvideo,perloader,pagination'

--- marcusblog/routes.py
+++ marcusblog/routes.py
@@ -10,7 +10,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    page=request.args.get('page',1, type=int)
+    posts = Post.query.paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 
@@ -126,3 +127,14 @@ def update_post(post_id):
             form.title.data = post.title
             form.content.data = post.content
     return render_template('create_post.html', title = 'update Post', form=form, legend='Update Post')
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted', 'success')
+    return redirect(url_for('home'))
