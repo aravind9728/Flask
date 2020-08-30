5780acdb6e916997ff6eaa0e3c025376ce7e2646
aravind9728 <aravind.sekar9797@gmail.com>
2020-08-31 01:35:13 +0530

    'errors

--- marcusblog/users/utils.py
+++ marcusblog/users/utils.py
@@ -3,7 +3,8 @@ import secrets
 from PIL import Image
 from flask import url_for
 from flask_mail import Message
-from marcusblog import app, mail
+from marcusblog import mail
+from flask import current_app
 
 
 
@@ -11,7 +12,7 @@ def save_picture(from_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(from_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
     from_picture.save(picture_path)
 
     output_size = (125, 125)
